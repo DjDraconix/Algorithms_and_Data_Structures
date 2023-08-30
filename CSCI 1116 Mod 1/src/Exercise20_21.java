17ec3ec64bad5324aa12b36244526c23bdc74b00
404kevnotfound <137447786+DjDraconix@users.noreply.github.com>
2023-08-30 14:41:59 -0600

    atempting to get the program to search the smallest value

--- CSCI 1116 Mod 1/src/Exercise20_21.java
+++ CSCI 1116 Mod 1/src/Exercise20_21.java
@@ -13,16 +13,22 @@ public class Exercise20_21 {
 				new Rectangle(4, 65), new Circle(4.5), new Rectangle(4.4, 1),
 				new Circle(6.5), new Rectangle(4, 5)};
 
+		@SuppressWarnings("unused")
 		Circle[] list1 = {new Circle(2), new Circle(3), new Circle(2),
 				new Circle(5), new Circle(6), new Circle(1), new Circle(2),
 				new Circle(3), new Circle(14), new Circle(12)};
-		selectionSort(list1, new GeometricObjectComparator());
-		for (int i = 0; i < list1.length; i++)
-			System.out.println(list1[i].getArea() + " ");
+		
+		selectionSort(list, new GeometricObjectComparator());
+		
+		for (int i = 0; i < list.length; i++)
+			System.out.println(list[i].getArea() + " ");
 	}
 
 	public static <E> void selectionSort(E[] list, Comparator<? super E> comparator) {
-		
+		E[] temperary = list;
+		for (int i = 0; i < list.length; i++) {
+			
+		}
 	}
 	
 }
