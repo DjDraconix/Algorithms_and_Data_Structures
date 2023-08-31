e8e955d9712aa9ba256ba9a1382ec03b71e30766
404kevnotfound <137447786+DjDraconix@users.noreply.github.com>
2023-08-31 09:42:27 -0600

    Compleated Exercise20-21

--- CSCI 1116 Mod 1/src/Exercise20_21.java
+++ CSCI 1116 Mod 1/src/Exercise20_21.java
@@ -17,18 +17,23 @@ public class Exercise20_21 {
 		Circle[] list1 = {new Circle(2), new Circle(3), new Circle(2),
 				new Circle(5), new Circle(6), new Circle(1), new Circle(2),
 				new Circle(3), new Circle(14), new Circle(12)};
-		
+
 		selectionSort(list, new GeometricObjectComparator());
-		
+
 		for (int i = 0; i < list.length; i++)
 			System.out.println(list[i].getArea() + " ");
 	}
 
 	public static <E> void selectionSort(E[] list, Comparator<? super E> comparator) {
-		E[] temperary = list;
-		for (int i = 0; i < list.length; i++) {
-			
+		//loop through list
+		for (int i = 0; i < list.length - 1; i++) {
+			for (int j = i + 1; j < list.length; j++) {
+				if(comparator.compare(list[i], list[j]) > 0) {
+					E temp = list[i];
+					list[i] = list[j];
+					list[j] = temp;
+				}
+			}
 		}
 	}
-	
 }
